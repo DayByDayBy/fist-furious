882eedef0a2a731943b526bccd200948c61f0b49
ABDURSHAZAM <85063702+ABDURSHAZAM@users.noreply.github.com>
2024-02-24 18:40:55 +0000

    dumping my shit here

---
+++ 3d Hand Tracking/Assets/Scripts/DemoBoxSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoBoxSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject boxPrefab;
+
+    [SerializeField]
+    float timerLength;
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > timerLength)
+        {
+            GameObject newBox = Instantiate(boxPrefab, new Vector3(0, 5, 0), Quaternion.identity);
+            Destroy(newBox, 5);
+            timer = 0;
+        }
+    }
+}
