635e2be5676cfe34a86b8f6c3b218c7e92ed72e2
RJW19 <148894701+RJW19@users.noreply.github.com>
2024-02-24 22:16:49 +0000

    computer says woah

---
+++ 3d Hand Tracking/Assets/Scripts/RossColliderWin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RossColliderWin : MonoBehaviour
+{   
+    public Transform scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene = GameObject.Find("AllSceneObjects").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Ball")
+        {   
+            SceneManager.LoadScene(0);
+            
+            print("Ball hole");
+        }
+    }
+}
