725519291a7cec7621200e7b271803df31b75cef
ABDURSHAZAM <85063702+ABDURSHAZAM@users.noreply.github.com>
2024-02-25 00:36:13 +0000

    James Level Done and Added

---
+++ 3d Hand Tracking/Assets/Scripts/JamesKey.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JamesKey : MonoBehaviour
+{
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Finger Tip")
+        {
+            audioSource.PlayOneShot(audioSource.clip);
+            JamesLevelScript.wasAKeyPressed = true;
+        }
+    }
+}
---
+++ 3d Hand Tracking/Assets/Scripts/JamesLevelScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JamesLevelScript : MonoBehaviour
+{
+    public float timer;
+    public float timerLength;
+    public static bool wasAKeyPressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > timerLength)
+        {
+            print("Loading Main Menu");
+            SceneManager.LoadScene(0);
+        }
+        if (wasAKeyPressed)
+        {
+            wasAKeyPressed = false;
+            timer = 0f;
+        }
+
+    }
+}
